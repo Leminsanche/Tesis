d8791144cc2f91e292134b5d4ac592f58cf3862b
Leminsanche <nicolas.sanchez.a@usach.cl>
2023-08-17 12:05:08 -0400

    Mejoras Lanzador

--- Lanzador2.py
+++ Lanzador2.py
@@ -11,7 +11,8 @@ def Demiray():
     a,b = random.uniform(0.02,0.1) , random.uniform(1,5)
     desplazamientos, tensiones, gradientes, malla = Vulcan().Biaxial_Demiray(a,b)
     grad = gradientes[0]
-    return
+
+    return [desplazamientos], [tensiones], [gradientes], malla
 
 
 def Yeoh():
@@ -19,18 +20,38 @@ def Yeoh():
     desplazamientos, tensiones, gradientes, malla = Vulcan().Biaxial_Yeoh(a,b,c)
     grad = gradientes[0]
 
-    return 
+    return [desplazamientos], [tensiones], [gradientes], malla
 
 
 if __name__ == '__main__':
     import sys 
     
     modelo = sys.argv[1]
-    numero_simulaciones = sys.argv[2]
+    numero_simulaciones = int(sys.argv[2])
     if modelo == 'Demiray':
-        Demiray()
+        Nombre_info_salida = '_Demiray20_Biaxial_'  ## Nombre caso EJ: _Tracion_Demiray_
+        Ubicacion_salida = 'Resultados/Biaxial_Demiray_20/'
+        for i in range(numero_simulaciones):
+            print(f'####################################### Simulacion {i+1}/{numero_simulaciones} #######################################')
+            numero_guardado = i
+            desplazamientos, tensiones, gradientes, malla = Demiray()
+            nombre_T = 'Tensiones' + Nombre_info_salida + str(numero_guardado) + '.npz' 
+            nombre_D = 'Desplazamientos' + Nombre_info_salida  + str(numero_guardado) + '.npz'
+            nombre_G = 'Gradientes' + Nombre_info_salida  + str(numero_guardado) + '.npz'
+
+            save_result(nombre_T ,nombre_D,nombre_G,Ubicacion_salida,desplazamientos,tensiones,gradientes)
 
     elif modelo == 'Yeoh':
-        Yeoh()
+        Nombre_info_salida = '_Yeoh20_Biaxial_'  ## Nombre caso EJ: _Tracion_Demiray_
+        Ubicacion_salida = 'Resultados/Biaxial_Yeoh_20/'
+        for i in range(numero_simulaciones):
+            print(f'####################################### Simulacion {i+1}/{numero_simulaciones} #######################################')
+            numero_guardado = i
+            desplazamientos, tensiones, gradientes, malla = Demiray()
+            nombre_T = 'Tensiones' + Nombre_info_salida + str(numero_guardado) + '.npz' 
+            nombre_D = 'Desplazamientos' + Nombre_info_salida  + str(numero_guardado) + '.npz'
+            nombre_G = 'Gradientes' + Nombre_info_salida  + str(numero_guardado) + '.npz'
+
+            save_result(nombre_T ,nombre_D,nombre_G,Ubicacion_salida,desplazamientos,tensiones,gradientes)
 
 
